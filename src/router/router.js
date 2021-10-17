99c96483ed9d65293a8268b0b34515fc80ca2f33
Yilee <rita841027@gmail.com>
2021-10-17 22:26:30 +0800

    修改內容

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from 'vue-router';
+
 Vue.use(VueRouter);
 
 const routes = [
@@ -24,8 +25,6 @@ const routes = [
 
 
 const router = new VueRouter({
-    mode: 'history',
-    base: '/',
     routes
 });
 
